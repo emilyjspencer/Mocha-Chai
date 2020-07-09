56c5b1301fcc3c8941bf18034c6d64f9110ebb00
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-09 21:13:32 +0100

    Adds the creation of three notes to the beforeEach to DRY UP code

--- backend/notes.test.js
+++ backend/notes.test.js
@@ -7,27 +7,21 @@ describe('Notes', () => {
 
   beforeEach(() => {
     noteApp = new Notes();
+    noteApp.createNote('Finish learning Mocha and Chai', 9);
+    noteApp.createNote('Learn how to integrate React with Rails', 4)
+    noteApp.createNote('Build the rest of the Meditation app', 6);
   })
 
   it('adds a new note to the notes array', () =>  {
-    noteApp.createNote('Finish learning Mocha and Chai');
-    noteApp.createNote('Learn how to integrate React with Rails')
-    noteApp.createNote('Build the rest of the Meditation app');
     expect(noteApp.notes.length).to.equal(3);
   });
 
   it('the note has an id and content', () => {
-    noteApp.createNote('Finish learning Mocha and Chai');
-    noteApp.createNote('Learn how to integrate React with Rails')
-    noteApp.createNote('Build the rest of the Meditation app');
     expect(noteApp.notes[0].id).to.equal(0);
     expect(noteApp.notes[1].content).to.equal('Learn how to integrate React with Rails');
   });
 
   it('the note has an id, content, date and priority number', () => {
-    noteApp.createNote('Finish learning Mocha and Chai', 9);
-    noteApp.createNote('Learn how to integrate React with Rails', 4)
-    noteApp.createNote('Build the rest of the Meditation app', 6);
     expect(noteApp.notes[2].priorityNumber).to.equal(6);
     expect(noteApp.notes[1].date).not.to.equal(undefined);
   });
