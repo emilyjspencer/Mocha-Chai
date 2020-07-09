59abfa059849f0f57f6611e4a024a26149359daa
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-10 00:13:24 +0100

    Removes dead code

--- backend/notes.js
+++ backend/notes.js
@@ -12,7 +12,6 @@ class Notes {
 
     createNote = (content, priorityNumber) => {
       let noteItem = {
-        //id: this.notes.length,
         id: newId(this.notes),
         content: content,
         priorityNumber: priorityNumber,
@@ -23,20 +22,15 @@ class Notes {
     }
 
     get = (id) => {
-      //return this.notes[id];
       return this.notes.filter(note => note.id === id)[0]
     }
 
     delete(id) {
       this.notes = this.notes.filter(note => note.id !== id)
-      //let found = this.notes[id]
-      //return this.notes.pop(found);
       return this.notes;
     }
 
     editNote(id, newContent) {
-      //this.notes[id].content = newContent;
-      //return this.notes[id];
       let index = this.notes.findIndex(note => note.id === id);
       this.notes[index].content = newContent;
     };
