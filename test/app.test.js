a22d7da7ded55da2de3eac8d9a9e238ae3987d0d
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-09 16:09:28 +0100

    Adds mocha and chai. Adds package-lock.json and node_modules to .gitignore. Writes first passing test

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+let assert = require('assert');
+
+describe('the length of the string', () => {
+
+  it('should return the number of characters in a string', () => {
+    assert.equal('Emily is learning how to use Mocha and Chai'.length, 43);
+  });
+
+});
